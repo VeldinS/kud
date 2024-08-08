982508abf47b640e8d7471612fa9ad229a7422a2
VeldinS <veldinsalcinovic123@gmail.com>
2024-08-08 17:10:39 +0200

    Dynamic background for whole apge

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,18 @@
 import Navbar from "@/components/navbar/Navbar";
 import Landing from "@/components/home/Landing";
 
-import background from '@/assets/Background.svg';
+import {BackgroundGradientAnimation} from "@/components/ui/background-gradient";
+import {About} from "@/components/home/About";
 
 export default function Home() {
     return (
-        <div style={{
-            backgroundImage: `url(${background})`,
-            backgroundSize: 'contain',
-            backgroundPosition: 'center',
-        }}>
+        <div>
+            <div className={'fixed top-0 bottom-0 left-0 right-0 z-0'}>
+                <BackgroundGradientAnimation></BackgroundGradientAnimation>
+            </div>
             <Navbar />
             <Landing />
+            <About />
         </div>
     );
 }
