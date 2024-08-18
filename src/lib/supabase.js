c96da8d81038e14cd714c0e3a9ae1f2c052b61af
VeldinS <veldinsalcinovic123@gmail.com>
2024-08-18 20:45:49 +0200

    Supabase login/logout logic

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,5 +4,29 @@ const supabaseUrl = 'https://iuabsbrilpyfeoswppwd.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YWJzYnJpbHB5ZmVvc3dwcHdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMyMDY5MzcsImV4cCI6MjAzODc4MjkzN30.GDebfcNNazr05hRJbXgHGLXXFy9jO-7gmLZImPy8tEw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-console.log('Connected successfully to database!');
 
+export const getUser = async () => {
+	const { data: { user }, error } = await supabase.auth.getUser();
+	console.log('Retrieved User:', user);
+	if (error) {
+		console.error('Error retrieving user:', error);
+	}
+	return user;
+};
+
+
+export const signInWithEmail = async (email, password) => {
+	const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+
+	if (data?.session?.access_token) {
+		document.cookie = `user=${JSON.stringify(data.session.user)}; path=/`;
+	}
+
+	return { data, error };
+};
+
+
+export const signOut = async () => {
+	const { error } = await supabase.auth.signOut();
+	return { error };
+};
